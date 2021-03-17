6f54e25fe04edc4e049e404ca540c0ba0b190220
Mário Viegas <m.diogoviegas98@ua.pt>
2021-03-17 02:37:50 +0000

    Finished

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var weather = require('openweather-apis');
-let city = 'Lisboa';
-let temperature;
+const http = require('http');
+const { parse } = require('querystring');
 let apiKey = '34bc4e52df32eb5758d9e67bd2b39e43';
-	weather.setLang('pt');
+
+
 	// English - en, Russian - ru, Italian - it, Spanish - es (or sp),
 	// Ukrainian - uk (or ua), German - de, Portuguese - pt,Romanian - ro,
 	// Polish - pl, Finnish - fi, Dutch - nl, French - fr, Bulgarian - bg,
@@ -11,47 +12,76 @@ let apiKey = '34bc4e52df32eb5758d9e67bd2b39e43';
 
 
 	// set city by name
-	weather.setCity(city);
 
 	// 'metric'  'internal'  'imperial'
- 	weather.setUnits('metric');
 
 	// check http://openweathermap.org/appid#get for get the APPID
- 	weather.setAPPID(apiKey);
 
-    weather.getTemperature(function(err, temp){
-		temperature = temp;
-        console.log(temp);
-	});
-  let http = require('http');
 
-  let handleRequest = (request, response) => {
-    var html = [
-        '<!DOCTYPE html>',
-        '<html>',
-            '<head>',
-                '<meta charset="utf-8" />',
-                '<title>Sample Response</title>',
-            '</head>',
-            '<body>',
-                '<div style="background-color: lightgray; width: 120px; padding: 5px; padding-left:30px ;border-radius: 15px">',
-                '<h1>',
-                city,
-                '</h1>',
-                '<h4>',
-                temperature,
-                'ºC',
-                '</h4>',
-                '</div>',
-            '</body>',
-        '</html>'
-    ].join('');
-      response.writeHead(200, {
-        'Content-Length': Buffer.byteLength(html, 'utf8'),
-        'Content-Type': 'text/html;'
-      });
-      response.write(html, 'utf8');
-      response.end();
-  };
-  
-  http.createServer(handleRequest).listen(8000);
+    const server = http.createServer((req, res) => {
+        
+        if (req.method === 'POST') {
+            collectRequestData(req, result => {
+                weather.setLang('pt');
+                weather.setAPPID(apiKey);
+                weather.setUnits('metric');
+                weather.setCity(result.city);
+                weather.getTemperature(function(err, temp){
+                    res.end(`
+                    <!doctype html>
+                    <html>
+                        <head>
+                            <meta charset="utf-8" />
+                            <title>Sample Response</title>
+                        </head>
+                    <body>
+                        <div style="background-color: lightgray; width: 120px; padding: 5px; padding-left:30px ;border-radius: 15px">
+                        <h1>
+                            ${result.city}
+                        </h1>
+                        <h4>
+                            ${temp} ºC
+                        </h4>
+                    </div>
+                        </form>
+                    </body>
+                    </html>
+                `);
+                });
+            });
+        }else{
+            res.end(`
+                <!doctype html>
+                <html>
+                    <head>
+                        <meta charset="utf-8" />
+                        <title>Sample Response</title>
+                    </head>
+                <body>
+                    <form action="/" method="post">
+                        <input type="text" name="city" /><br />
+                        <button>Check</button>
+                    </form>
+                </body>
+                </html>
+            `);
+        }
+    });
+    server.listen(8000);
+
+    function collectRequestData(request, callback) {
+        const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+        if(request.headers['content-type'] === FORM_URLENCODED) {
+            let body = '';
+            request.on('data', chunk => {
+                body += chunk.toString();
+            });
+            request.on('end', () => {
+                callback(parse(body));
+            });
+        }
+        else {
+            callback(null);
+        }
+    }
+
